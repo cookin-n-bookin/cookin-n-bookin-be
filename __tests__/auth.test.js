00dbd6acd5bddd59ec8ed879ab08ec10153998e8
Denzel <dbartolaba123@gmail.com>
2022-05-25 12:35:46 -0700

    created the routes to controller for auth, and created test for signup

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -11,4 +11,13 @@ describe('cookin-n-bookin-be routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+
+  it('Should be able to sign up a user', async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({ username: 'dobby', password: 'chicken' });
+
+    expect(res.body).toEqual({ id: expect.any(String), username: 'dobby' });
+  });
 });
