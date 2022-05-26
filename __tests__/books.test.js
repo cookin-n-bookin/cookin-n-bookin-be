6d17c30f3c5cc0e9e2f4dc1609cb5fb1d7b43da6
Denzel <dbartolaba123@gmail.com>
2022-05-26 10:31:10 -0700

    created test for getting all the books

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Book = require('../lib/models/Book');
 
 
 describe('book routes', () => {
@@ -27,4 +28,20 @@ describe('book routes', () => {
       ...book,
     });
   })
+
+  it('should get all of the books', async () => {
+    const book = await Book.insert({
+      title: 'cookin',
+      author: 'bookin',
+      imageId: expect.any(String),
+    });
+
+    const res = await request(app)
+      .get('/api/v1/books');
+    expect(res.body).toEqual([
+      {
+        id: expect.any(String),
+        ...book
+      }]);
+  });
 });
