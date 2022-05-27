63a91108f73923b6a14c99ed97e0c8fc057f437f
Denzel <dbartolaba123@gmail.com>
2022-05-27 11:05:12 -0700

    created the rest of our SQL tables need to get junction tables working

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,6 +3,9 @@
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS books CASCADE;
 DROP TABLE IF EXISTS books_users CASCADE;
+DROP TABLE IF EXISTS recipes CASCADE;
+DROP TABLE IF EXISTS notes CASCADE;
+DROP TABLE IF EXISTS users_recipes CASCADE;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -21,3 +24,33 @@ CREATE TABLE books_users (
   book_id BIGINT REFERENCES books(id),
   user_id BIGINT REFERENCES users(id)
 );
+
+CREATE TABLE recipes (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title TEXT NOT NULL,
+  book_id BIGINT REFERENCES books(id),
+  page_number BIGINT,
+  ingredients TEXT [],
+  rating SMALLINT,
+  image_id TEXT
+);
+
+CREATE TABLE notes (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  content TEXT NOT NULL,
+  private BOOLEAN,
+  user_id BIGINT REFERENCES users(id),
+  recipe_id BIGINT REFERENCES books(id)
+);
+
+CREATE TABLE users_recipes (
+  user_id BIGINT REFERENCES users(id),
+  recipe_id BIGINT REFERENCES recipes(id),
+  cooked BOOLEAN
+
+);
+
+
+
+
+
