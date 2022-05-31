87e363697d3683aa6a96bdf973f8514ef49cd458
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-31 16:57:48 -0700

    wrote test for get all notes

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -55,4 +55,51 @@ describe('recipe routes', () => {
     });
   });
 
+  it('should get all notes for a recipe by recipe id', async () => {
+    const newUser = {
+      username: 'dobby2',
+      password: 'chicken',
+    };
+    
+    const agent = request.agent(app);
+    await agent
+      .post('/api/v1/users/signup')
+      .send(newUser);
+    
+    const recipe = {
+      title: 'Hot Dog',
+      bookId: '1',
+      pageNumber: '40',
+      ingredients: ['buns', 'hotdog', 'mustard', 'jalapenos'],
+      rating: 5,
+      imageId: 'this is a hotdog'
+    };
+    
+    request.agent(app);
+    await agent
+      .post('/api/v1/recipes')
+      .send(recipe);
+  
+    const note = {
+      content: 'bake til cooked', 
+      isPrivate: false,
+      userId: '1',
+      recipeId: '1'
+    };
+  
+    await agent
+      .post('/api/v1/notes')
+      .send(note);
+
+    const res = await agent
+      .get('/api/v1/notes');
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      ...note
+    });
+
+
+  });
+
 });
