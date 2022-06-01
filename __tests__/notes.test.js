f3815691c6adf3136ed58a148762a1182e7746dd
Denzel <dbartolaba123@gmail.com>
2022-05-31 17:44:54 -0700

    finished writing tests and controller

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Note = require('../lib/models/Note');
 
 
 describe('recipe routes', () => {
@@ -150,5 +151,56 @@ describe('recipe routes', () => {
 
     expect(res.body).toEqual(expected);
   })
+
+  it('Should be able to delete a note', async () => {
+    const newUser = {
+      username: 'dobby2',
+      password: 'chicken',
+    };
+
+    const agent = request.agent(app);
+    await agent
+      .post('/api/v1/users/signup')
+      .send(newUser);
+
+    const recipe = {
+      title: 'Hot Dog',
+      bookId: '1',
+      pageNumber: '40',
+      ingredients: ['buns', 'hotdog', 'mustard', 'jalapenos'],
+      rating: 5,
+      imageId: 'this is a hotdog'
+    };
+
+    request.agent(app);
+    await agent
+      .post('/api/v1/recipes')
+      .send(recipe);
+
+    const note = await agent
+      .post('/api/v1/notes')
+      .send({
+        content: 'This book is about billiards!!!!!',
+        isPrivate: false,
+        userId: '1',
+        recipeId: '1'
+      });
+
+
+    const getAllNotes = await Note.getAll();
+
+    const res = await agent
+      .delete(`/api/v1/notes/${note.id}`)
+  });
+
+  expect(res.body).toEqual({
+    id: expect.any(String),
+    content: 'This book is about billiards!!!!!',
+    isPrivate: false,
+    userId: '1',
+    recipeId: '1'
+  });
+  const getAllNotes2 = await Note.getAll();
+  expect(getAllNotes2.length).toEqual(getAllNotes.length - 1);
 
 });
