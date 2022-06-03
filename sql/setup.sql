b95d02bfade0b36cdc0401d2cacf4c74a6f09be1
Trevor Rezac <trev.rezac@gmail.com>
2022-06-02 19:44:30 -0700

    updated sql data to include images

--- sql/setup.sql
+++ sql/setup.sql
@@ -54,22 +54,22 @@ CREATE TABLE users_recipes (
 INSERT INTO 
 books (title, author, image_id)
 VALUES
-('Foodheim', 'Eric Wareheim', 'CLOUDINARY_ID_HERE'), --1 
-('Cook This Book', 'Molly Baz', 'CLOUDINARY_ID_HERE'), --2
-('Xi''an Famous Foods', 'Jason Wang', 'CLOUDINARY_ID_HERE'), --3
-('Dessert Person', 'Claire Saffitz', 'CLOUDINARY_ID_HERE'), --4
-('Nothing Fancy', 'Alison Roman', 'CLOUDINARY_ID_HERE'), --5
-('That Sounds So Good', 'Carla Lalli Music', 'CLOUDINARY_ID_HERE'); --6
+('Foodheim', 'Eric Wareheim', 'rryvwl6wktkziup0kbca'), --1 
+('Cook This Book', 'Molly Baz', 'nugksb4tqxfmvxdittdf'), --2
+('Xi''an Famous Foods', 'Jason Wang', 'xi_an_famous_foods_t3rrou'), --3
+('Dessert Person', 'Claire Saffitz', 'dessert_person_b3nikr'), --4
+('Nothing Fancy', 'Alison Roman', 'nothing_fancy_zhrstd'), --5
+('That Sounds So Good', 'Carla Lalli Music', 'that_sounds_so_good_m17afz'); --6
 
 INSERT INTO
 recipes (title, book_id, page_number, ingredients, rating, image_id)
 VALUES
-('Emilio''s Pomodoro', 1, 117, ['dried spaghetti', 'extra-virgin olive oil', 'garlic cloves', 'cherry tomatoes', 'kosher salt', 'fresh basil', 'freshly grated Parmigiano-Reggiano', 'flaky sea salt' ], 5, null),
-('Brussels Sprouts with Shrimp Sauce', 3, 76, ['vegetable oil', 'garlic cloves', 'Brussels sprouts', 'salt', 'dried shrimp', 'chicken bouillon powder'], 3, null),
-('Honey Tahini Challah', 4, 295, ['active dry yeast', 'honey', 'tahini', 'large egg yolks', 'extra-virgin olive oil', 'large eggs', 'bread flour', 'Diamond Crystal kosher salt', 'sesame seeds'], null),
-('Gingery Ground Beef with Lime and Herbs', 6, 60, ['cooked rice', 'ginger', 'garlic cloves', 'shallot', 'limes', 'soy sauce', 'sriracha', 'fish sauce', 'vegetable oil', '80% lean ground beef', 'kosher salt', 'cilantro'], null),
-('Big Shells with Escarole, ''Chovies & Mozz', 2, 156, ['escarole or radicchio or kale', 'garlic cloves', 'oregano or thyme leaves', 'fresh whole-milk mozzarella', 'heavy cream', 'Parmesan cheese', 'kosher salt', 'extra-virgin olive oil', 'panko bread crumbs', 'oil-packed anchovy fillets', 'red pepper flakes', 'dried large pasta shells'], null),
-('Garlicky Broccoli and Greens with Hazelnut and Coriander', 5, 160, ['garlic cloves', 'toasted hazelnuts', 'coriander seeds', 'olive oil', 'kosher salt', 'broccoli', 'lacinato kale', 'lemon', 'flaky sea salt'], null);
+('Emilio''s Pomodoro', 1, 117, ARRAY ['dried spaghetti', 'extra-virgin olive oil', 'garlic cloves', 'cherry tomatoes', 'kosher salt', 'fresh basil', 'freshly grated Parmigiano-Reggiano', 'flaky sea salt'], 5, null),
+('Brussels Sprouts with Shrimp Sauce', 3, 76, ARRAY ['vegetable oil', 'garlic cloves', 'Brussels sprouts', 'salt', 'dried shrimp', 'chicken bouillon powder'], 3, null),
+('Honey Tahini Challah', 4, 295, ARRAY ['active dry yeast', 'honey', 'tahini', 'large egg yolks', 'extra-virgin olive oil', 'large eggs', 'bread flour', 'Diamond Crystal kosher salt', 'sesame seeds'], 4, null),
+('Gingery Ground Beef with Lime and Herbs', 6, 60, ARRAY ['cooked rice', 'ginger', 'garlic cloves', 'shallot', 'limes', 'soy sauce', 'sriracha', 'fish sauce', 'vegetable oil', '80% lean ground beef', 'kosher salt', 'cilantro'], 5, null),
+('Big Shells with Escarole, ''Chovies & Mozz', 2, 156, ARRAY ['escarole or radicchio or kale', 'garlic cloves', 'oregano or thyme leaves', 'fresh whole-milk mozzarella', 'heavy cream', 'Parmesan cheese', 'kosher salt', 'extra-virgin olive oil', 'panko bread crumbs', 'oil-packed anchovy fillets', 'red pepper flakes', 'dried large pasta shells'], 2, null),
+('Garlicky Broccoli and Greens with Hazelnut and Coriander', 5, 160, ARRAY ['garlic cloves', 'toasted hazelnuts', 'coriander seeds', 'olive oil', 'kosher salt', 'broccoli', 'lacinato kale', 'lemon', 'flaky sea salt'], 3, null);
 
 
 
