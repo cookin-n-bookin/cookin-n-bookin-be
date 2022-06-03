6bc337b9fd0e5417eadb535f53d4300f2553a2c4
Trevor Rezac <trev.rezac@gmail.com>
2022-06-02 21:43:45 -0700

    changes to sql table data

--- sql/setup.sql
+++ sql/setup.sql
@@ -30,7 +30,7 @@ CREATE TABLE recipes (
   title TEXT NOT NULL,
   book_id BIGINT REFERENCES books(id),
   page_number BIGINT,
-  ingredients TEXT [],
+  ingredients TEXT,
   rating SMALLINT,
   CONSTRAINT rating CHECK (rating BETWEEN 1 AND 5),
   image_id TEXT
@@ -47,8 +47,6 @@ CREATE TABLE notes (
 CREATE TABLE users_recipes (
   user_id BIGINT REFERENCES users(id),
   recipe_id BIGINT REFERENCES recipes(id),
-  cooked BOOLEAN
-
 );
 
 INSERT INTO 
@@ -64,12 +62,12 @@ VALUES
 INSERT INTO
 recipes (title, book_id, page_number, ingredients, rating, image_id)
 VALUES
-('Emilio''s Pomodoro', 1, 117, ARRAY ['dried spaghetti', 'extra-virgin olive oil', 'garlic cloves', 'cherry tomatoes', 'kosher salt', 'fresh basil', 'freshly grated Parmigiano-Reggiano', 'flaky sea salt'], 5, null),
-('Brussels Sprouts with Shrimp Sauce', 3, 76, ARRAY ['vegetable oil', 'garlic cloves', 'Brussels sprouts', 'salt', 'dried shrimp', 'chicken bouillon powder'], 3, null),
-('Honey Tahini Challah', 4, 295, ARRAY ['active dry yeast', 'honey', 'tahini', 'large egg yolks', 'extra-virgin olive oil', 'large eggs', 'bread flour', 'Diamond Crystal kosher salt', 'sesame seeds'], 4, null),
-('Gingery Ground Beef with Lime and Herbs', 6, 60, ARRAY ['cooked rice', 'ginger', 'garlic cloves', 'shallot', 'limes', 'soy sauce', 'sriracha', 'fish sauce', 'vegetable oil', '80% lean ground beef', 'kosher salt', 'cilantro'], 5, null),
-('Big Shells with Escarole, ''Chovies & Mozz', 2, 156, ARRAY ['escarole or radicchio or kale', 'garlic cloves', 'oregano or thyme leaves', 'fresh whole-milk mozzarella', 'heavy cream', 'Parmesan cheese', 'kosher salt', 'extra-virgin olive oil', 'panko bread crumbs', 'oil-packed anchovy fillets', 'red pepper flakes', 'dried large pasta shells'], 2, null),
-('Garlicky Broccoli and Greens with Hazelnut and Coriander', 5, 160, ARRAY ['garlic cloves', 'toasted hazelnuts', 'coriander seeds', 'olive oil', 'kosher salt', 'broccoli', 'lacinato kale', 'lemon', 'flaky sea salt'], 3, null);
+('Emilio''s Pomodoro', 1, 117, 'dried spaghetti, extra-virgin olive oil, garlic cloves, cherry tomatoes, kosher salt, fresh basil, freshly grated Parmigiano-Reggiano, flaky sea salt', 5, null),
+('Brussels Sprouts with Shrimp Sauce', 3, 76, 'vegetable oil, garlic cloves, Brussels sprouts, salt, dried shrimp, chicken bouillon powder', 3, null),
+('Honey Tahini Challah', 4, 295, 'active dry yeast, honey, tahini, large egg yolks, extra-virgin olive oil, large eggs, bread flour, Diamond Crystal kosher salt, sesame seeds', 4, null),
+('Gingery Ground Beef with Lime and Herbs', 6, 60, 'cooked rice, ginger, garlic cloves, shallot, limes, soy sauce, sriracha, fish sauce, vegetable oil, 80% lean ground beef, kosher salt, cilantro', 5, null),
+('Big Shells with Escarole, Chovies & Mozz', 2, 156, 'escarole or radicchio or kale, garlic cloves, oregano or thyme leaves, fresh whole-milk mozzarella, heavy cream, Parmesan cheese, kosher salt, extra-virgin olive oil, panko bread crumbs, oil-packed anchovy fillets, red pepper flakes, dried large pasta shells', 2, null),
+('Garlicky Broccoli and Greens with Hazelnut and Coriander', 5, 160, 'garlic cloves, toasted hazelnuts, coriander seeds, olive oil, kosher salt, broccoli, lacinato kale, lemon, flaky sea salt', 3, null);
 
 
 
